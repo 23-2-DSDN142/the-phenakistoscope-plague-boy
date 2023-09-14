385082996b83784583e06419483e36484726a3e2
plague-boy <tommmysmith.204@gmail.com>
2023-09-14 15:00:53 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -3,10 +3,10 @@ const SLICE_COUNT = 10;
 function setup_pScope(pScope){
  //pScope.output_mode(STATIC_DISK);
  //pScope.output_mode(STATIC_FRAME);
- pScope.output_mode(ANIMATED_DISK);
- //pScope.output_mode(OUTPUT_GIF(200))
+ //pScope.output_mode(ANIMATED_DISK);
+ pScope.output_mode(OUTPUT_GIF(1000))
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
   pScope.load_image_sequence("zeus" , "png", 14)
